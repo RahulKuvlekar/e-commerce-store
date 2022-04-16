4a18161338ebb27e81b21eb54e104ce0dea06a56
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-04-16 19:58:19 +0530

    Feat - Added Products page

---
+++ src/Components/ProductListing/ProductListing.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./ProductListing.css";
+
+const ProductListing = () => {
+  return (
+    <div className="products-section">
+      <h1 className="page-title">Showing Products</h1>
+      <div className="main-section grid"></div>
+    </div>
+  );
+};
+
+export default ProductListing;
