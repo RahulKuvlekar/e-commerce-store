79a6b1000d74389b5df071209d75bf76328b3462
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-04-16 20:00:06 +0530

    Feat - Added Carts Page

---
+++ src/Pages/Cart/Cart.js
@@ -0,0 +1,21 @@
+import React from "react";
+import CartCard from "../../Components/CartCard/CartCard";
+import CartListing from "../../Components/CartListing/CartListing";
+import PricingSection from "../../Components/PricingSection/PricingSection";
+import "./Cart.css";
+
+const Cart = () => {
+  return (
+    <div>
+      <h1 className="page-title page-title-center">My Cart</h1>
+      <div className="cart grid">
+        <div className="cart-section">
+          <CartListing />
+        </div>
+        <PricingSection />
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
