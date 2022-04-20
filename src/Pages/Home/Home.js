92a8db6e41520a541fc26532c918a013db27e323
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-04-20 16:00:16 +0530

    Update - Added categories

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,19 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import "./Home.css";
 import Carousel from "../../Components/UI/Carousel/Carousel";
 import { carouselData } from "../../Components/UI/Carousel/carouselData";
 import RowSlider from "../../Components/UI/RowSilder/RowSlider";
-import { categories } from "../../backend/db/categories";
-import "./Home.css";
+import { useProductContext } from "../../Hooks/useProductContext";
 
 const Home = () => {
+  const { productState } = useProductContext();
+  const { categoryList } = productState;
   return (
     <div>
       <Carousel data={carouselData} height="90vh" width="100vw" />
-      <RowSlider title="Category" data={categories} card={true} />
+      {categoryList?.length > 0 && (
+        <RowSlider title="Category" data={categoryList} card={true} />
+      )}
     </div>
   );
 };
