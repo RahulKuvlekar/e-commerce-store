8353b6cefd924d1f25bccc1717e724b0c9248dfd
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-05-01 04:52:58 +0530

    Update - Added AuthContext API

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { ProductProvider } from "./Context/productContext";
+import AuthProvider from "./Context/AuthContext";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ProductProvider>
-        <App />
-      </ProductProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <ProductProvider>
+          <App />
+        </ProductProvider>
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
