bdbedeb2bc35e0a7c132b1fbbc63e5a29e7a37a0
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2022-05-09 20:51:08 +0530

    Update - Added Brand Row Slider

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,16 +3,30 @@ import Carousel from "../../Components/UI/Carousel/Carousel";
 import { carouselData } from "../../Components/UI/Carousel/carouselData";
 import RowSlider from "../../Components/UI/RowSilder/RowSlider";
 import { useProductContext } from "../../Hooks/useProductContext";
+import { brand } from "../../backend/db/brand";
 
 const Home = () => {
   const { productState } = useProductContext();
   const { categoryList } = productState;
 
+  const rowSliderData = categoryList.map(({ categoryName, categoryImage }) => ({
+    name: categoryName,
+    img: categoryImage,
+  }));
+
+  const brandRowSliderData = brand.map(({ brandName, brandImage }) => ({
+    name: brandName,
+    img: brandImage,
+  }));
+
   return (
     <div>
       <Carousel data={carouselData} height="90vh" width="100vw" />
       {categoryList?.length > 0 && (
-        <RowSlider title="Category" data={categoryList} card={true} />
+        <RowSlider title="Category" data={rowSliderData} card={true} />
+      )}
+      {brand?.length > 0 && (
+        <RowSlider title="Brand" data={brandRowSliderData} card={false} />
       )}
     </div>
   );
---
+++ src/backend/db/brand.js
@@ -0,0 +1,29 @@
+import { v4 as uuid } from "uuid";
+
+export const brand = [
+  {
+    _id: uuid(),
+    brandName: "H&M",
+    brandImage: "/Assets/BrandCarousel/H&M-Logo.svg",
+  },
+  {
+    _id: uuid(),
+    brandName: "Zara",
+    brandImage: "/Assets/BrandCarousel/Zara-Logo.svg",
+  },
+  {
+    _id: uuid(),
+    brandName: "Nike",
+    brandImage: "/Assets/BrandCarousel/Nike-Logo.svg",
+  },
+  {
+    _id: uuid(),
+    brandName: "GShock",
+    brandImage: "/Assets/BrandCarousel/g-shock-casio.svg",
+  },
+  {
+    _id: uuid(),
+    brandName: "Fastrack",
+    brandImage: "/Assets/BrandCarousel/Fastrack-Logo.svg",
+  },
+];
